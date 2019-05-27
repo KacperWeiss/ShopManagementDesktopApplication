5fcabbf7dcf635b60b4c2c6aaf4fe89d878cb3b7
Studenny Kacper <kacper.studenny@gmail.com>
2019-05-27 17:05:53 +0200

    Added Wholesalers accessor

---
+++ ShopAccessApp/ShopAccessApp/BackEnd/WarehouseOrderAccessor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAccessApp.BackEnd
+{
+    public class WarehouseOrderAccessor
+    {
+        public List<warehouse_orders> GetAllWarehouseOrder()
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                return db.warehouse_orders.ToList<warehouse_orders>();
+            }
+        }
+
+        #region get
+        public warehouse_orders GetWholesalersByWholesalerID(int id)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                return db.warehouse_orders.SingleOrDefault(t => t.id == id);
+            }
+        }
+
+        public warehouse_orders GetWholesalersByStatus(short status)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                return db.warehouse_orders.SingleOrDefault(t => t.status == status);
+            }
+        }
+
+        public wholesalers GetWholesalersByName(string companyName)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                return db.wholesalers.SingleOrDefault(t => t.company == companyName);
+            }
+        }
+        #endregion
+
+        public void CreateWarehouseOrder(warehouse_orders order)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                db.warehouse_orders.Add(order);
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteWholesalerByID(int id)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                warehouse_orders order = db.warehouse_orders.SingleOrDefault(x => x.id == id);
+
+                var entry = db.Entry(order);
+                if (entry.State == System.Data.Entity.EntityState.Detached)
+                    db.warehouse_orders.Attach(order);
+                db.warehouse_orders.Remove(order);
+                db.SaveChanges();
+            }
+        }
+    }
+}
