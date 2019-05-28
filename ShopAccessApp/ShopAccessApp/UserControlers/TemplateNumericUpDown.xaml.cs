9e5ec61add6738a8ad47f2a63013fbf2eef592b4
Kacper Weiss <kacper.weiss@gmail.com>
2019-05-28 14:37:54 +0200

    Numeric up and down contol

---
+++ ShopAccessApp/ShopAccessApp/UserControlers/TemplateNumericUpDown.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UserControlTemplate
+{
+    /// <summary>
+    /// Interaction logic for TemplateNumericUpDown.xaml
+    /// </summary>
+    public partial class TemplateNumericUpDown : UserControl
+    {
+        private int _numValue = 0;
+        public TemplateNumericUpDown()
+        {
+            InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+        }
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _numValue = value;
+                    txtNum.Text = value.ToString();
+                }
+            }
+        }
+
+        private void CmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+        }
+
+        private void CmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+        }
+
+        private void TxtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
+    }
+}
