07548066c693b590893b1658587b6c0864f25d07
Kacper Weiss <kacper.weiss@gmail.com>
2019-05-31 10:07:41 +0200

    CaseEntry added

---
+++ ShopAccessApp/ShopAccessApp/UserControlers/ProductEntries/CaseEntry.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShopAccessApp.UserControlers.ProductEntries
+{
+    /// <summary>
+    /// Interaction logic for CaseEntry.xaml
+    /// </summary>
+    public partial class CaseEntry : UserControl
+    {
+        public CaseEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
