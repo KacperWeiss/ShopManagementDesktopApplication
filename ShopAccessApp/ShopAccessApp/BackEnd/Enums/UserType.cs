a2226cf584914d9223d8a22daa848ea9e332fd32
Kacper Weiss <kacper.weiss@gmail.com>
2019-06-09 17:35:41 +0200

    Added user login and register methods within UserManagement Class

---
+++ ShopAccessApp/ShopAccessApp/BackEnd/Enums/UserType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAccessApp.BackEnd.Enums
+{
+    public enum UserType
+    {
+        Admin = 0,
+        Seller,
+        WarehouseKeeper,
+        Technician
+    }
+}
---
+++ ShopAccessApp/ShopAccessApp/BackEnd/Logics/UserManagement.cs
@@ -0,0 +1,45 @@
+﻿using ShopAccessApp.BackEnd.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAccessApp.BackEnd.Logics
+{
+    static public class UserManagement
+    {
+        static public UserType LoginAs(string login, string password)
+        {
+            users localUser = UserAccessor.GetByUsername(login);
+            if (localUser.password == password)
+            {
+                return (UserType)localUser.access_level;
+            }
+            else
+            {
+                throw new Exception("Wrong password!");
+            }
+        }
+
+        static public UserType RegisterAs(string login, string password, string registrationKey)
+        {
+            var localRegistration = RegistrationAccessor.GetByActivationCode(registrationKey);
+            if (localRegistration != null)
+            {
+                UserAccessor.CreateNew(new users()
+                {
+                    username = login,
+                    password = password,
+                    access_level = localRegistration.access_level
+                });
+                RegistrationAccessor.DeleteByActivationCode(registrationKey);
+                return (UserType)localRegistration.access_level;
+            }
+            else
+            {
+                throw new Exception($"Registration Key {registrationKey} does not exist!");
+            }
+        }
+    }
+}
