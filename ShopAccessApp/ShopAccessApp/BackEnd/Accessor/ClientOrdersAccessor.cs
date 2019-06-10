31ad2d2e75a5b4d8a4087ebec7b14e705f503eff
Kacper Weiss <kacper.weiss@gmail.com>
2019-06-10 13:37:45 +0200

    Added more methods to ClientOrdersAccessor

--- ShopAccessApp/ShopAccessApp/BackEnd/Accessor/ClientOrdersAccessor.cs
+++ ShopAccessApp/ShopAccessApp/BackEnd/Accessor/ClientOrdersAccessor.cs
@@ -15,5 +15,27 @@ namespace ShopAccessApp.BackEnd.Accessor
                 return db.client_order_sets.ToList<client_order_sets>();
             }
         }
+
+        static public client_order_sets GetById(int id)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                return db.client_order_sets.SingleOrDefault(t => t.id == id);
+            }
+        }
+
+        static public void DeleteById(int id)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                client_order_sets client_order_set = db.client_order_sets.SingleOrDefault(x => x.id == id);
+
+                var entry = db.Entry(client_order_set);
+                if (entry.State == System.Data.Entity.EntityState.Detached)
+                    db.client_order_sets.Attach(client_order_set);
+                db.client_order_sets.Remove(client_order_set);
+                db.SaveChanges();
+            }
+        }
     }
 }
