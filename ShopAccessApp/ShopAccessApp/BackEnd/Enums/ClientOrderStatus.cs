a5c916ad62a95d0c41ca410d8473cde36d99b0ab
Kacper Weiss <kacper.weiss@gmail.com>
2019-06-10 14:48:42 +0200

    Added techinicainTabsManagement prototype

---
+++ ShopAccessApp/ShopAccessApp/BackEnd/Enums/ClientOrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAccessApp.BackEnd.Enums
+{
+    enum ClientOrderStatus
+    {
+        ReadyToOrder = 0,
+        Ordered,
+        ServiceCompleted,
+        Delivered
+    }
+}
