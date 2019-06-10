f9dd79d95c99988f69ab2965b7847aab7264d3cf
Kacper Weiss <kacper.weiss@gmail.com>
2019-06-10 14:13:51 +0200

    RegistrationKeyGeneration added

---
+++ ShopAccessApp/ShopAccessApp/BackEnd/Logics/RegistrationKeyGeneration.cs
@@ -0,0 +1,41 @@
+﻿using ShopAccessApp.BackEnd.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAccessApp.BackEnd.Logics
+{
+    static public class RegistrationKeyGeneration
+    {
+        static public string GetRandomKey()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[20];
+            var random = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    stringChars[i*7 + j] = chars[random.Next(chars.Length)];
+                }
+            }
+            stringChars[7] = '-';
+            stringChars[14] = '-';
+
+            return new String(stringChars);
+        }
+
+        static public void SaveToDatabase(string key, UserType userType)
+        {
+            RegistrationAccessor.CreateNew(new Registration()
+            {
+                access_level = (short)userType,
+                activation_code = key
+            });
+        }
+    }
+}
