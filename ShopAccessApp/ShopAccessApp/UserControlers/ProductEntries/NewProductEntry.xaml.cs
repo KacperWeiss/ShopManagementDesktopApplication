b4411cc5b10cfae68418fdd7c8945ba2ee03b683
Kamil Szprych <kkamil4sz@gmail.com>
2019-06-11 01:27:48 +0200

    NewProductEntry implemented completly with small bug (default image not showing up)

--- ShopAccessApp/ShopAccessApp/UserControlers/ProductEntries/NewProductEntry.xaml.cs
+++ ShopAccessApp/ShopAccessApp/UserControlers/ProductEntries/NewProductEntry.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,17 +29,73 @@ namespace ShopAccessApp.UserControlers.ProductEntries
 
         private void AddProductButton_Click(object sender, RoutedEventArgs e)
         {
+            int index = ProductTypeSelectionComboBox.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    AddMotherboard();
+                    break;
+                case 1:
+                    AddProcessor();
+                    break;
+                case 2:
+                    AddRamMemory();
+                    break;
+                case 3:
+                    AddGraphicsCard();
+                    break;
+                case 4:
+                    AddCase();
+                    break;
+                default:
+                    break;
+            }
+            ClearTextBoxes();
+        }
 
+        private void ClearTextBoxes()
+        {
+            ModelNameTextBox.Text = "";
+            BrandNameTextBox.Text = "";
+            ProcessorSocketTypeTextBox.Text = "";
+            RamMemoryTypeTextBox.Text = "";
+            RamMemoryCapacityTextBox.Text = "";
+            RamMemorySlotsTextBox.Text = "";
+            PriceTextBox.Text = "";
+            LinkToImageTextBox.Text = "";
+            DescriptionTextBox.Text = "";
         }
 
         private void AddMotherboard()
         {
-            
+            motherboards motherboard = new motherboards()
+            {
+                model = ModelNameTextBox.Text,
+                cpu_socket = ProcessorSocketTypeTextBox.Text,
+                ram_memory_type = RamMemoryTypeTextBox.Text,
+                ram_memory_slots = Int32.Parse(RamMemorySlotsTextBox.Text),
+                price = Int32.Parse(PriceTextBox.Text),
+                amount = 0,
+                image_source = LinkToImageTextBox.Text,
+                description = DescriptionTextBox.Text
+            };
+
+            MotherBoardsAcessor.CreateNew(motherboard);
         }
 
         private void AddProcessor()
         {
+            processors processor = new processors()
+            {
+                model = ModelNameTextBox.Text,
+                socket = ProcessorSocketTypeTextBox.Text,
+                price = Int32.Parse(PriceTextBox.Text),
+                amount = 0,
+                image_source = LinkToImageTextBox.Text,
+                description = DescriptionTextBox.Text
+            };
 
+            ProcessorsAccessor.CreateNew(processor);
         }
 
         private void AddGraphicsCard()
@@ -46,27 +103,131 @@ namespace ShopAccessApp.UserControlers.ProductEntries
             graphics_cards graphicsCard = new graphics_cards()
             {
                 model = ModelNameTextBox.Text,
-                brand = ,
-                price = temporaryGraphicsCard.price,
-                amount = amount,
-                image_source = temporaryGraphicsCard.image_source,
-                description = temporaryGraphicsCard.description
+                brand = BrandNameTextBox.Text,
+                price = Int32.Parse(PriceTextBox.Text),
+                amount = 0,
+                image_source = LinkToImageTextBox.Text,
+                description = DescriptionTextBox.Text
             };
+
+            GraphicsCardsAccessor.CreateNew(graphicsCard);
         }
 
         private void AddRamMemory()
         {
+            ram_memories ramMemory = new ram_memories()
+            {
+                model = ModelNameTextBox.Text,
+                type = RamMemoryTypeTextBox.Text,
+                capacity_gb = Int32.Parse(RamMemoryCapacityTextBox.Text),
+                price = Int32.Parse(PriceTextBox.Text),
+                amount = 0,
+                image_source = LinkToImageTextBox.Text,
+                description = DescriptionTextBox.Text
+            };
 
+            RamMemoriesAccessor.Create(ramMemory);
         }
 
         private void AddCase()
         {
+            cases caseInstance = new cases()
+            {
+                model = ModelNameTextBox.Text,
+                price = Int32.Parse(PriceTextBox.Text),
+                amount = 0,
+                image_source = LinkToImageTextBox.Text,
+                description = DescriptionTextBox.Text
+            };
 
+            CasesAccessor.CreateNew(caseInstance);
         }
 
         private void ProductTypeSelectionComboBox_Loaded(object sender, RoutedEventArgs e)
         {
+            ProductTypeSelectionComboBox.SelectedIndex = 0;
+        }
 
+        private void ProductTypeSelectionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ProductTypeSelectionComboBox.SelectedIndex;
+
+            switch (index)
+            {
+                case 0:
+                    BrandField.Visibility = Visibility.Collapsed;
+                    ProcessorSocketTypeField.Visibility = Visibility.Visible;
+                    RamMemoryTypeField.Visibility = Visibility.Visible;
+                    RamMemoryCapacityField.Visibility = Visibility.Collapsed;
+                    RamMemorySlotsField.Visibility = Visibility.Visible;
+                    break;
+                case 1:
+                    BrandField.Visibility = Visibility.Visible;
+                    ProcessorSocketTypeField.Visibility = Visibility.Visible;
+                    RamMemoryTypeField.Visibility = Visibility.Collapsed;
+                    RamMemoryCapacityField.Visibility = Visibility.Collapsed;
+                    RamMemorySlotsField.Visibility = Visibility.Collapsed;
+                    break;
+                case 2:
+                    BrandField.Visibility = Visibility.Collapsed;
+                    ProcessorSocketTypeField.Visibility = Visibility.Collapsed;
+                    RamMemoryTypeField.Visibility = Visibility.Visible;
+                    RamMemoryCapacityField.Visibility = Visibility.Visible;
+                    RamMemorySlotsField.Visibility = Visibility.Collapsed;
+                    break;
+                case 3:
+                    BrandField.Visibility = Visibility.Visible;
+                    ProcessorSocketTypeField.Visibility = Visibility.Collapsed;
+                    RamMemoryTypeField.Visibility = Visibility.Collapsed;
+                    RamMemoryCapacityField.Visibility = Visibility.Collapsed;
+                    RamMemorySlotsField.Visibility = Visibility.Collapsed;
+                    break;
+                case 4:
+                    BrandField.Visibility = Visibility.Collapsed;
+                    ProcessorSocketTypeField.Visibility = Visibility.Collapsed;
+                    RamMemoryTypeField.Visibility = Visibility.Collapsed;
+                    RamMemoryCapacityField.Visibility = Visibility.Collapsed;
+                    RamMemorySlotsField.Visibility = Visibility.Collapsed;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void PriceTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void RamMemoryCapacityTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void RamMemorySlotsTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void LinkToImageTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("LOST FOCUS!!!!!!!");
+            if (!string.IsNullOrEmpty(LinkToImageTextBox.Text))
+            {
+                try
+                {
+                    var uri = new Uri(LinkToImageTextBox.Text);
+                    var bitmapImage = new BitmapImage(uri);
+                    ProductImage.Source = bitmapImage;
+                }
+                catch
+                {
+                    Console.WriteLine("EXCEPTION IMAGE!!!!!");
+                }
+            }
         }
     }
 }
