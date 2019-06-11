f8146ce94bd709d0e1dbed8b46e37d37cacc6dd5
Kamil Szprych <kkamil4sz@gmail.com>
2019-06-11 10:57:26 +0200

    Half implemented storage entries

--- ShopAccessApp/ShopAccessApp/UserControlers/StorageProductEntries/CaseStorageEntry.xaml.cs
+++ ShopAccessApp/ShopAccessApp/UserControlers/StorageProductEntries/CaseStorageEntry.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ShopAccessApp.BackEnd.Logics;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,61 @@ namespace ShopAccessApp.UserControlers.StorageProductEntries
     /// <summary>
     /// Interaction logic for CaseStorageEntry.xaml
     /// </summary>
-    public partial class CaseStorageEntry : UserControl
+    public partial class CaseStorageEntry : UserControl, INotifyPropertyChanged
     {
         public CaseStorageEntry()
         {
             InitializeComponent();
+        }
+
+        private int incoming = 0;
+        public int Incoming
+        {
+            get
+            {
+                return incoming;
+            }
+            set
+            {
+                if (incoming != value)
+                {
+                    incoming = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void StorageOrderCountTextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            var id = Convert.ToInt32(IDHolderHack.Text);
+
+
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                var total = db.warehouse_orders.Where(t => t.cases.id == id).ToList();
+                foreach (var item in total)
+                {
+                    Incoming += (int)item.case_amount;
+                }
+            }
+            StorageOrderCountTextBlock.Text = Incoming.ToString();
+
+        }
+
+        private void AddToCartButton_Click(object sender, RoutedEventArgs e)
+        {
+            int amount = AmountPicker.NumValue;
+            var caseInstance = (cases)DataContext;
+
+            //Zmienić na wharehouse order!!!!!
+            //ClientOrderManagement.AddCaseToOrder(caseInstance, amount);
+            WarehouseOrderManagement.AddCaseToOrder(caseInstance, amount);
         }
     }
 }
