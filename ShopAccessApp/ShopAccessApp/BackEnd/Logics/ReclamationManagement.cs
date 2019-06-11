366cccdf90b60b5436a43a1b207d8ee93ce332ae
Kacper Weiss <kacper.weiss@gmail.com>
2019-06-11 09:35:09 +0200

    ReclamationManagement added

--- ShopAccessApp/ShopAccessApp/BackEnd/Logics/ReclamationManagement.cs
+++ ShopAccessApp/ShopAccessApp/BackEnd/Logics/ReclamationManagement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopAccessApp.BackEnd.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,69 @@ namespace ShopAccessApp.BackEnd.Logics
 {
     static public class ReclamationManagement
     {
-        static public void FinalizeReclamation()
+        static public client_order_sets LocalOrder { get; set; } = new client_order_sets();
+        
+        static public void AddMotherboardToReclamation(motherboards temporaryMotherboard, int amount)
         {
-            throw new NotImplementedException();
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                LocalOrder.motherboards = db.motherboards.SingleOrDefault(t => t.id == temporaryMotherboard.id);
+                LocalOrder.id_motherboard = LocalOrder.motherboards.id;
+                LocalOrder.motherboard_amount = amount;
+            }
+        }
+
+        static public void AddProcessorToReclamation(processors temporaryProcessor, int amount)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                LocalOrder.processors = db.processors.SingleOrDefault(t => t.id == temporaryProcessor.id);
+                LocalOrder.id_processor = LocalOrder.processors.id;
+                LocalOrder.processor_amount = amount;
+            }
+        }
+
+        static public void AddRamMemoriesToReclamation(ram_memories temporaryRamMemory, int amount)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                LocalOrder.ram_memories = db.ram_memories.SingleOrDefault(t => t.id == temporaryRamMemory.id);
+                LocalOrder.id_ram_memory = LocalOrder.ram_memories.id;
+                LocalOrder.ram_memory_amount = amount;
+            }
+        }
+
+        static public void AddGraphicCardsToReclamation(processors temporaryGraphicCard, int amount)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                LocalOrder.graphics_cards = db.graphics_cards.SingleOrDefault(t => t.id == temporaryGraphicCard.id);
+                LocalOrder.id_graphics_card = LocalOrder.graphics_cards.id;
+                LocalOrder.graphics_card_amount = amount;
+            }
+        }
+
+        static public void AddCasesToReclamation(processors temporaryCase, int amount)
+        {
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                LocalOrder.cases = db.cases.SingleOrDefault(t => t.id == temporaryCase.id);
+                LocalOrder.id_case = LocalOrder.cases.id;
+                LocalOrder.case_amount = amount;
+            }
+        }
+
+        static public void FinalizeReclamation(clients client)
+        {
+            LocalOrder.clients = client;
+            LocalOrder.id_client = client.id;
+            LocalOrder.status = (short)ClientOrderStatus.Ordered;
+
+            using (var db = new StudiaProjektBazyDanychEntities())
+            {
+                LocalOrder.services = db.services.SingleOrDefault(t => t.service == "Reclamation service");
+                db.client_order_sets.Add(LocalOrder);
+            }
         }
     }
 }
